9c523dbdb2ff963e2398e6c21b0fb774efcbfd5e
Megaflinterino <steven.p.rudolf@outlook.de>
2024-12-02 16:03:52 +0100

    Added Part 2 of day 1

--- code01.12.2024.py
+++ code01.12.2024.py
@@ -1,8 +1,11 @@
+from collections import Counter
+
+
 #Creation of 2 empty lists a and b
 a, b = [], []
 
 #Opens the file with read properties as f and with makes sure the file is closed after
-with open('inputs01.12.2024.txt', 'r') as f:
+with open('/home/steven/AdventOfCode/AdventOfCode2024/inputs01.12.2024.txt', 'r') as f:
     #reads every line in the inpute and iterates it
     for lines in f.readlines():
         #The lines get split at spaces into x and y int(z) converts the numbers into integers
@@ -20,5 +23,12 @@ b.sort()
 print(a)
 print(b)
 
+#part1
 #The avsolute(so no value is negative) difference of every values gets added for i in range is used to iterate over every value len(a) or len(b) is irrelevant
 print(sum(abs(a[i]-b[i]) for i in range(len(a))))
+
+#part2
+
+c = Counter(b)
+
+print(sum(a[i] * c[a[i]]for i in range(len(a))))
